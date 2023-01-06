de9be0ba022d3423bf31f04405e5e8d263847689
toxin06 <53322305+toxin06@users.noreply.github.com>
2023-01-06 11:41:19 -0600

    [NPCs Hotfix] Add missing config option to display Vegas drops.

--- includes/config.template.php
+++ includes/config.template.php
@@ -73,6 +73,7 @@ $display_spawn_times          		= true; // If TRUE, will display the spawn times
 $show_npcs_difficulty				= false; // If TRUE, will show the difficulty of an NPCs
 $show_npcs_difficulty_search		= false; // If TRUE, adds a difficulty search to advanced NPCs
 $show_npcs_expansion_search			= false; // If TRUE, adds an expansion search to advanced NPCs
+$show_vegas_drops					= false; // If TRUE, shows Vegas drops on NPC pages.
 
 /**
  * Items
